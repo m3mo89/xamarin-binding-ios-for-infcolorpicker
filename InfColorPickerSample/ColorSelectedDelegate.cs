7c7e5fcb3c184d96870cb744a728df9e18a07808
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-10 16:41:46 -0600

    Subclass InfColorPickerControllerDelegate

---
+++ InfColorPickerSample/ColorSelectedDelegate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using InfColorPicker;
+using UIKit;
+
+namespace InfColorPickerSample
+{
+    public class ColorSelectedDelegate : InfColorPickerControllerDelegate
+    {
+        readonly UIViewController parent;
+
+        public ColorSelectedDelegate(UIViewController parent)
+        {
+            this.parent = parent;
+        }
+
+        public override void ColorPickerControllerDidFinish(InfColorPickerController controller)
+        {
+            parent.View.BackgroundColor = controller.ResultColor;
+            parent.DismissViewController(false, null);
+        }
+    }
+}
