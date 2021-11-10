7ee19cfc61a609cdec1d8aceedc5dfb7fc4ce0ae
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-10 16:46:51 -0600

    Implement the method HandleTouchUpInsideWithStrongDelegate

--- InfColorPickerSample/ViewController.cs
+++ InfColorPickerSample/ViewController.cs
@@ -1,4 +1,5 @@
 ﻿using Foundation;
+using InfColorPicker;
 using System;
 using UIKit;
 
@@ -6,6 +7,8 @@ namespace InfColorPickerSample
 {
     public partial class ViewController : UIViewController
     {
+        ColorSelectedDelegate selector;
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -14,12 +17,21 @@ namespace InfColorPickerSample
         {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+            ChangeColorButton.TouchUpInside += HandleTouchUpInsideWithStrongDelegate;
+            selector = new ColorSelectedDelegate(this);
         }
 
         public override void DidReceiveMemoryWarning()
         {
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void HandleTouchUpInsideWithStrongDelegate(object sender, EventArgs e)
+        {
+            InfColorPickerController picker = InfColorPickerController.ColorPickerViewController;
+            picker.Delegate = selector;
+            picker.PresentModallyOverViewController(this);
         }
     }
 }
