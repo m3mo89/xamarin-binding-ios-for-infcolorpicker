4c6abca85d9a96c25b2679d33b8f2eb7bd278e55
Guillermo Tinoco <m3mo89@gmail.com>
2021-11-10 15:11:16 -0600

    Create project for Static Library

---
+++ InfColorPicker/InfColorPicker/InfColorPicker.h
@@ -0,0 +1,12 @@
+//
+//  InfColorPicker.h
+//  InfColorPicker
+//
+//  Created by Guillermo Tinoco on 10/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InfColorPicker : NSObject
+
+@end
---
+++ InfColorPicker/InfColorPicker/InfColorPicker.m
@@ -0,0 +1,12 @@
+//
+//  InfColorPicker.m
+//  InfColorPicker
+//
+//  Created by Guillermo Tinoco on 10/11/21.
+//
+
+#import "InfColorPicker.h"
+
+@implementation InfColorPicker
+
+@end
